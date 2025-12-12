2c4ab5276afaea3e55a79a117a7b43204b9ab8be
Mithink <53110506+mithinkrishnappa@users.noreply.github.com>
2025-12-12 19:30:33 +0530

    deleted fabicon and also updated the router's

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
-import { App } from '../app/app';
-import { WelcomePage } from '../pages/welcomepage/welcomepage';
 
 export const routes: Routes = [
   {
     path: '',
-    component: App, // or whatever your default component is
-  },
-  {
-    path: 'welcome',
     loadComponent: () =>
       import('../pages/welcomepage/welcomepage').then((m) => m.WelcomePage),
   },
--- src/pages/welcomepage/welcomepage.ts
+++ src/pages/welcomepage/welcomepage.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome-page',
   standalone: true, // required for loadComponent
-  template: `
-    <h1>Welcome to Angular!</h1>
-    <p>This is your welcome page.</p>
-  `,
+  templateUrl: './welcomepage.html',
+  styleUrls: ['./welcomepage.css'],
 })
 export class WelcomePage {}
